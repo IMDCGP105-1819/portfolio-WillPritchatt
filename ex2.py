e821df69f5347575c88967213a94651f3f006460
Will Pritchatt <Will.pritchatt@btopenworld.com>
2018-09-27 17:30:30 +0100

    Second Question

---
+++ ex2.py
@@ -0,0 +1,29 @@
+print("These are how many animals I have:")
+
+# + adds two values together, / divides the value on the left by the value on the right
+print("Cows", 15 + 30 / 6)
+# - subtracts the value on the right from the value on the left,  * will multiply two values together
+# % will output the remainder of a division
+print("Sheep", 100 - 30 * 3 % 4)
+
+print("Now I will count my plants:")
+
+print(3 + 1 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 -7")
+# < is the less than operator, it will return true if the item on the left is less than the item on the right
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Can you see why that printed False earlier?")
+
+# > is the greater than operator, it will return true if the item on the left is greater than the item on the right
+print("Is it greater?", 5 > -2)
+# >= is the greater than or equal to operator, it will return true if the item on the left is greater than or equal to
+# the item on the right
+print("Is it greater or equal?", 5 >= -2)
+# <= is the less than or equal to operator, it will return true if the item on the left is less than or equal to the
+# item on the right
+print("Is it less or equal?", 5 <= -2)
