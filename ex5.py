a8c503207edeba1fb2fcd9dac84f03ca3c256438
Will Pritchatt <Will.Pritchatt@btopenworld.com>
2018-10-04 11:18:13 +0100

    Fifth Question, Context fix and comment

--- ex5.py
+++ ex5.py
@@ -1,8 +1,9 @@
 cards = 99
+#  While loop will run until the number of cards is equal to zero
 while cards != 0:
     print("{0} duel monsters cards on the wall, {0} duel monsters cards!".format(cards))
     cards -= 1
     if cards != 0:
         print("Take one down, trade it around {} duel monsters cards on the wall!".format(cards))
     else:
-        print("So take it down, pass it around, no more duel monsters cards on the wall!")
+        print("So take it down, trade it around, no more duel monsters cards on the wall!")
