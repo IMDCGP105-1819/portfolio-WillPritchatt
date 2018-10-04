dad77c12f39ac549650c895846117439d401e169
Will Pritchatt <Will.Pritchatt@btopenworld.com>
2018-10-04 11:47:41 +0100

    Sixth Question, Input functions added

--- ex6.py
+++ ex6.py
@@ -1,13 +1,22 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+height = input("Please enter your height in inches: ")
+weight = input("Please enter your weight in pounds: ")
+eyes = input("Please enter your eye colour: ")
+hair = input("Please enter your hair colour: ")
+is_heavy = weight > 3000
+
+
 # Constant to convert pounds into kilos
 POUNDS_TO_KILO = 0.45359237
 # Constant to convert inches into centimeters
 INCHES_TO_CM = 0.39370
 
-my_weight_kilo = round(my_weight * POUNDS_TO_KILO, 2)
-my_height_cm = round(my_height / INCHES_TO_CM, 2)
+my_weight_kilo = round(weight * POUNDS_TO_KILO, 2)
+my_height_cm = round(height / INCHES_TO_CM, 2)
 
-print("Let's talk about {}.".format(my_name))
-print("He is {} inches or {} centimeters tall.".format(my_height, my_height_cm))
-print("He is {} pounds or {} kilos heavy.".format(my_weight, my_weight_kilo))
+print("Let's talk about {}.".format(name))
+print("He is {} inches or {} centimeters tall.".format(height, my_height_cm))
+print("He is {} pounds or {} kilos heavy.".format(weight, my_weight_kilo))
 print("It is {} that he is overweight.".format(is_heavy))
-print("He's got {} eyes and {} hair.".format(my_eyes, my_hair))
+print("He's got {} eyes and {} hair.".format(eyes, hair))
