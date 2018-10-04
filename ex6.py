e1e6ce3f063a97aa4ccadb63bb9a8f6406d80922
Will Pritchatt <Will.Pritchatt@btopenworld.com>
2018-10-04 11:49:39 +0100

    Sixth Question, Added int conditions to height and weight

--- ex6.py
+++ ex6.py
@@ -1,7 +1,7 @@
 name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
-height = input("Please enter your height in inches: ")
-weight = input("Please enter your weight in pounds: ")
+height = int(input("Please enter your height in inches: "))
+weight = int(input("Please enter your weight in pounds: "))
 eyes = input("Please enter your eye colour: ")
 hair = input("Please enter your hair colour: ")
 is_heavy = weight > 3000
