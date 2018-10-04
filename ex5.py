94e07845e63671c8bb393be6ede040d6cfd3366d
Will Pritchatt <Will.Pritchatt@btopenworld.com>
2018-10-04 11:13:39 +0100

    Fifth Question, While Loop Draft

---
+++ ex5.py
@@ -0,0 +1,5 @@
+cards = 99
+while cards != 0:
+    print("{} duel monsters cards on the wall, {} duel monsters cards!".format(cards))
+    cards -= 1
+    print("Take one down, trade it around {} duel monsters cards on the wall!".format(cards))
