c7349041fc7c73b65a655d16af02066ece3aad9f
Will Pritchatt <Will.Pritchatt@btopenworld.com>
2018-10-04 11:17:00 +0100

    Fifth Question, If Condition for 0 cards left

--- ex5.py
+++ ex5.py
@@ -2,4 +2,7 @@ cards = 99
 while cards != 0:
     print("{0} duel monsters cards on the wall, {0} duel monsters cards!".format(cards))
     cards -= 1
-    print("Take one down, trade it around {} duel monsters cards on the wall!".format(cards))
+    if cards != 0:
+        print("Take one down, trade it around {} duel monsters cards on the wall!".format(cards))
+    else:
+        print("So take it down, pass it around, no more duel monsters cards on the wall!")
