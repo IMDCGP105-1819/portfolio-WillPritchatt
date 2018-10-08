4c28242f4371b52199b479c2e79d9eb4e0db39bf
Will Pritchatt <Will.pritchatt@btopenworld.com>
2018-10-08 19:24:32 +0100

    Question 7, Adding and initialising variables and constants

---
+++ ex7.py
@@ -0,0 +1,8 @@
+PORTION_DEPOSIT = 0.2
+current_savings = 0
+R = 0.04
+print("Enter the following information in Pounds, you do not need to include the sign")
+total_cost = float(input("Please enter the total cost of the house: "))
+annual_salary = float(input("Please enter your annual salary: "))
+portion_saved = input("Please enter the percentage you want to save for down payment as a decimal: ")
+
