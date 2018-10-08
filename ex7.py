63e9afe2192554e57e22dae78c87bc0133041c16
Will Pritchatt <Will.pritchatt@btopenworld.com>
2018-10-08 20:17:45 +0100

    Question 7, Bugs fixed, Removed For and uncommented While Loop, Output added

--- ex7.py
+++ ex7.py
@@ -3,21 +3,16 @@ R = 0.04
 print("Enter the following information in Pounds, you do not need to include the sign")
 total_cost = float(input("Please enter the total cost of the house: "))
 annual_salary = float(input("Please enter your annual salary: "))
-portion_saved = input("Please enter the percentage of your salary that you want to save, as a decimal: ")
+portion_saved = float(input("Please enter the percentage of your salary that you want to save, as a decimal: "))
 
 portion_deposit = total_cost * 0.2
-print(portion_deposit)
 monthly_salary = annual_salary / 12
-print(monthly_salary)
 
 count = 0
 
-#while current_savings < portion_deposit
-for i in range(10):
-    current_savings = current_savings * R / 12
-    print("Return Investment: ", current_savings)
-    current_savings = current_savings + monthly_salary
+while current_savings < portion_deposit:
+    current_savings += current_savings * R / 12
+    current_savings += monthly_salary * portion_saved
     count += 1
-    print(current_savings, count)
 
-
+print("It will take you {} months to save for the deposit".format(count))
