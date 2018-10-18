22e4479deaca4424d53fa4948ab91bce9b197e17
WillPritchatt <will.pritchatt@btopenworld.com>
2018-10-18 11:58:56 +0100

    Question 11, Final

--- ex12.py
+++ ex12.py
@@ -6,13 +6,14 @@ def hotel_cost(nights):
 def plane_ticket_cost(city, class_multiplier):
     if city == "New York":
         cost = 2000 * class_multiplier
-    elif city == "Auckalnd":
+    elif city == "Auckland":
         cost = 790 * class_multiplier
     elif city == "Venice":
         cost = 154 * class_multiplier
     elif city == "Glasgow":
         cost = 65 * class_multiplier
     return cost
+
 
 def rental_car(days):
     cost = days * 30
@@ -22,3 +23,34 @@ def rental_car(days):
         cost -= 50
     return cost
 
+
+def total_cost(city, days):
+    cost = 0
+
+    cost += hotel_cost(days)
+
+    print(""""Travel Class
+    1. Economy
+    2. Premium Economy
+    3. Business
+    4. First Class
+    """)
+    travel_class = int(input("Please enter the travel class (Identify using the number): "))
+    class_multiplier = 1 + (travel_class - 1) * 0.3
+
+    cost += plane_ticket_cost(city, class_multiplier)
+    cost += rental_car(days)
+
+    print("The final cost is £{}". format(cost))
+
+
+print("""Cites to travel to
+1. New York
+2. Auckland
+3. Venice
+4. Glasgow
+""")
+city = input("Please enter the name of the City you are travelling to (Identify using the name): ")
+days = int(input("Enter the number of days you are staying: "))
+
+total_cost(city, days)
