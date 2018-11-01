f2021100e302f9d2eacbf26e53c0139bc9ae45e5
Will Pritchatt <will.pritchatt@btopenworld.com>
2018-11-01 11:54:51 +0000

    Lyric Frequency, find lyric by frequency found

--- Lyric.py
+++ Lyric.py
@@ -11,8 +11,12 @@ def add_lyrics(frequency, song):
     return frequency
 
 
-def user_menu():
-    pass
+def search_frequencies(frequencey, freq_search):
+    words = []
+    for lyric, freq in frequency.items():
+        if freq >= freq_search:
+            words.append(lyric)
+    print(words)
 
 
 frequency = {}
@@ -22,8 +26,11 @@ song = "Cold! The air and water flowing. Hard! The land we call our home. Push! 
        "call our home. Push! To keep the dark from coming. Feel! The weight of what we owe. This! The song of sons " \
        "and daughters. Hide! The heart of who we are. Making peace to build a future. Strong united working 'til we " \
        "fall. And we all lift, and we're all adrift, Together, together. Through the cold mist, 'til we're lifeless" \
-       "Together, together" # We All Lift Together - Keith Power
+       "Together, together"  # We All Lift Together - Keith Power
 song = song.lower()
 frequency = add_lyrics(frequency, song)
 print(frequency)
-
+freq_search = -1
+while freq_search != 0:
+    freq_search = int(input("Enter the minimum frequency of the words you want to find (0 to quit): "))
+    search_frequencies(frequency, freq_search)
